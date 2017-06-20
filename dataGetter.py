7e6716c752f2084af004c6406e640d134b635e01
Duc Thong Nguyen <duc.thong.nguyen.88@gmail.com>
2017-06-20 02:08:48 +0100

    Cleaning up data getter

--- dataGetter.py
+++ dataGetter.py
@@ -1,12 +1,17 @@
+import urllib.request
+import pandas as pd
+import datetime
+import zipfile
+
 def getData(date):
     link = 'http://images1.cafef.vn/data/'+date.strftime('%Y%m%d') + \
     '/CafeF.SolieuGD.Upto'+date.strftime('%d%m%Y') +'.zip'
-    #'http://images1.cafef.vn/data/20170303/CafeF.SolieuGD.03032017.zip'
-    #http://images1.cafef.vn/data/20170303/CafeF.SolieuGD.Upto03032017.zip
     fileName = 'D:\\python\\vnstock\\history.zip'
-    req = urllib2.urlopen(link)
+    #req = urllib2.urlopen(link)
+    response = urllib.request.urlopen(link)
+
     file = open(fileName, 'wb')
-    file.write(req.read())
+    file.write(response.read())
     file.close()
     exchanges = ['HSX','HNX']
     list_ =[]
@@ -14,7 +19,11 @@ def getData(date):
         for exchange in exchanges:
             with myzip.open('CafeF.'+exchange+'.Upto'+date.strftime('%d.%m.%Y')+'.csv') as myfile:
                 data = pd.read_csv(myfile, header = 0,names = ['Ticker','Date','Open','High','Low','Close','Volume'])
+                data['Exchange'] = exchange
                 list_.append(data)
     frame = pd.concat(list_)
     frame['Date'] = pd.to_datetime(frame.Date, format='%Y%m%d')
     return(frame)
+
+#data = getData(datetime.datetime(2017,3,31,0,0))
+#print(data)
